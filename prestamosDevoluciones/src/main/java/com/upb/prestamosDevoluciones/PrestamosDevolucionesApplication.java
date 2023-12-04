97b1fcdfce9d5665e93177100635881a40d03eb7
Paul Landaeta <paul.landaeta-macbook@Pauls-MacBook-Pro-2.local>
2023-12-04 14:23:36 -0400

    devoluciones y prestamos

---
+++ prestamosDevoluciones/src/main/java/com/upb/prestamosDevoluciones/PrestamosDevolucionesApplication.java
@@ -0,0 +1,13 @@
+package com.upb.prestamosDevoluciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrestamosDevolucionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrestamosDevolucionesApplication.class, args);
+	}
+
+}
