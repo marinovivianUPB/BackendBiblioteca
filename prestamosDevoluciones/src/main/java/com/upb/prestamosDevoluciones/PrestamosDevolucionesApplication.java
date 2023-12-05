56d489e031cbfa5455b52d91fcb8b4ded11ec2db
Paul Landaeta <paul.landaeta-macbook@Pauls-MacBook-Pro-2.local>
2023-12-05 14:24:29 -0400

    services

--- prestamosDevoluciones/src/main/java/com/upb/prestamosDevoluciones/PrestamosDevolucionesApplication.java
+++ prestamosDevoluciones/src/main/java/com/upb/prestamosDevoluciones/PrestamosDevolucionesApplication.java
@@ -2,8 +2,10 @@ package com.upb.prestamosDevoluciones;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.FeignClient;
 
 @SpringBootApplication
+@FeignClient("/libros")
 public class PrestamosDevolucionesApplication {
 
 	public static void main(String[] args) {
